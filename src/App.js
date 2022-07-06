5fd556709041a9b3c551cc9b44111763497f47f5
Ali Bingöl <33944377+alibingoll@users.noreply.github.com>
2022-07-06 10:55:21 +0300

    Component Drilling

--- src/App.js
+++ src/App.js
@@ -2,52 +2,72 @@ import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Container, Row } from "react-bootstrap";
-import { Component } from "react";
-export default class App extends Component {
-  state = { currentCategory: "", products: [] };
+import {Component} from "react";
+import {Col, Container, Row} from "reactstrap";
 
-  changeCategory = (category) => {
-    this.setState({ currentCategory: category.categoryName });
-    this.getProducts(category.id)
-  };
-  componentDidMount(){
-    this.getProducts();
-  }
-  getProducts = (categoryId) => {
-      let url =  "http://localhost:3000/products";
-      if(categoryId){
-          url+="?categoryId="+categoryId
-      }
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => this.setState({ products: data }));
-  };
-  render() {
-    let productInfo = { title: "Product List" };
-    let categoryInfo = { title: "Category List" };
-    return (
-      <Container>
-        <Row>
-          <Navi></Navi>
-        </Row>
-        <Row>
-          <Col md={"3"}>
-            <CategoryList
-              currentCategory={this.state.currentCategory}
-              changeCategory={this.changeCategory}
-              info={categoryInfo}
-            ></CategoryList>
-          </Col>
-          <Col md={"9"}>
-            <ProductList
-              products={this.state.products}
-              currentCategory={this.state.currentCategory}
-              info={productInfo}
-            ></ProductList>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+export default class App extends Component {
+    state = {currentCategory: "", products: [],cart:[]};
+
+    changeCategory = (category) => {
+        this.setState({currentCategory: category.categoryName});
+        this.getProducts(category.id)
+    };
+
+    componentDidMount() {
+        this.getProducts();
+    }
+
+    getProducts = (categoryId) => {
+        let url = "http://localhost:3000/products";
+        if (categoryId) {
+            url += "?categoryId=" + categoryId
+        }
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => this.setState({products: data}));
+    };
+    addToCart = (product) => {
+        let newCart = this.state.cart;
+        var addedItem = newCart.find(c=>c.product.id===product.id);
+        if(addedItem){
+            addedItem.quantity+=1
+        }else{
+            newCart.push({product:product,quantity:1})
+        }
+        this.setState({cart:newCart})
+    };
+    removeFromCart=(product) =>{
+        let newCart = this.state.cart.filter(c=>c.product.id!==product.id)
+        this.setState({cart:newCart})
+    }
+
+    render() {
+        let productInfo = {title: "Product List"};
+        let categoryInfo = {title: "Category List"};
+        return (
+            <Container>
+                <Navi
+                    cart={this.state.cart}
+                    removeFromCart={this.removeFromCart}
+                ></Navi>
+                <Row>
+                    <Col md={"3"}>
+                        <CategoryList
+                            currentCategory={this.state.currentCategory}
+                            changeCategory={this.changeCategory}
+                            info={categoryInfo}
+                        ></CategoryList>
+                    </Col>
+                    <Col md={"9"}>
+                        <ProductList
+                            products={this.state.products}
+                            currentCategory={this.state.currentCategory}
+                            addToCart={this.addToCart}
+                            info={productInfo}
+                        ></ProductList>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,34 +1,35 @@
-import { Component } from "react";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
+import {Component} from "react";
+import {ListGroup, ListGroupItem} from "reactstrap";
 
 export default class CategoryList extends Component {
-  state = {
-    categories: []
-  };
-  componentDidMount(){
-    this.getCategories();
-  }
+    state = {
+        categories: []
+    };
 
-  getCategories = ()=>{
-    fetch("http://localhost:3000/categories")
-    .then(response=>response.json())
-    .then(data=> this.setState({categories:data}))
-   
-  }
+    componentDidMount() {
+        this.getCategories();
+    }
 
-  render() {
-    return (
-      <div>
-        <h2>{this.props.info.title}</h2>
-        <ListGroup>
-        {this.state.categories.map((category) => (
-            <ListGroupItem onClick={()=>this.props.changeCategory(category)} key={category.id}>
-              {category.categoryName}
-            </ListGroupItem>
-          ))}
-        </ListGroup>
-        {/* <h4>{this.props.currentCategory}</h4> */}
-      </div>
-    );
-  }
+    getCategories = () => {
+        fetch("http://localhost:3000/categories")
+            .then(response => response.json())
+            .then(data => this.setState({categories: data}))
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.props.info.title}</h2>
+                <ListGroup>
+                    {this.state.categories.map((category) => (
+                        <ListGroupItem onClick={() => this.props.changeCategory(category)} key={category.id}>
+                            {category.categoryName}
+                        </ListGroupItem>
+                    ))}
+                </ListGroup>
+                {/* <h4>{this.props.currentCategory}</h4> */}
+            </div>
+        );
+    }
 }
